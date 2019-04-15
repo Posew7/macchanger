57ee1a964afa7aab62d71dbaaee68ae4aff3b88a
posew7 <posew7@gmail.com>
2019-04-15 13:40:58 +0100

    subprocess optparse macchanger

--- hw_ether.py
+++ hw_ether.py
@@ -1,7 +1,14 @@
 import subprocess
+import optparse
 
-mac = input("Yeni Mac Adresini Girin : ")
-interface = input("Değiştirmek İstediğin Interface Girin : ")
+parse = optparse.OptionParser()
+parse.add_option("-i","--interface",dest="interface",help="interface to change")
+parse.add_option("-m","--mac",dest="mac",help="new mac adress")
+
+(user_inputs,arguments) = parse.parse_args()
+
+interface = user_inputs.interface
+mac = user_inputs.mac
 
 subprocess.call(["ifconfig",interface,"down"])
 subprocess.call(["ifconfig",interface,"hw","ether",mac])
